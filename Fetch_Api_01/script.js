46dd8eeda826897cb0da09694af3608239c673ee
mvargasvega <martinvargasvega@gmail.com>
2019-05-20 22:21:08 -0700

    Created a script that will access information of a CSV file and then return only the year and the temperature of that year

---
+++ Fetch_Api_01/script.js
@@ -0,0 +1,33 @@
+console.log("Sanity Check")
+
+//call the function
+getData();
+
+async function getData() {
+    //fetch's the data and assigns it to response
+    const response = await fetch('ZonAnn.Ts+dSST.csv');
+
+    // console.log(response);
+    const data = await response.text();
+
+    // backslash n, is for a line break
+    //we use slice because we don't need the first row, just the data
+    const table = data.split('\n').slice(1);
+    table.forEach(row => {
+        // console.log(element);
+        const columns = row.split(',');
+        const year = columns[0];
+        const temp = columns[1];
+
+        // console.log(row);
+        console.log(year,temp);
+    });
+};
+
+/*
+Getting the raw data as text,
+
+Spliititng it up
+Putting into a variable call table
+Going
+*/
